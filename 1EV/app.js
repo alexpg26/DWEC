9668ed2c31eb2418ac63dad724e357be43ab22c2
alex <you@example.com>
2023-09-21 17:37:37 +0200

    Mensaje de guardar archivos

---
+++ 1EV/app.js
@@ -0,0 +1,35 @@
+console.log("Hola mundo");  //mensaje general
+        console.info('Esto es un mensaje'); //Información
+        console.warn('Cuidadin');   //Aviso
+        console.error('Error fatal');   //Error 
+        //alert('Mensaje');
+        //confirm('Confirmación');
+        //prompt('Mensaje');
+        var num=5; //variable tradicional visible en todo el script
+        let numero=8; //variable accesible sólo a su scope
+        const pi=3.14;  //constante accesible a su scope
+        /*
+        
+        Number: Número
+        String: Cadena
+        Boolean:
+        NaN: Not a Number
+        Undefined: No definido
+        Null: Nulo
+
+        */
+       /*
+
+        Operadores aritméticos + - * /
+
+       */
+      /*
+
+        Operadores de comparación, entre los que tenemos: 
+
+        <>
+        <= o >=
+        == o !=
+        === o !==
+
+      */
